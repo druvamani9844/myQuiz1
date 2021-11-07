a8361a8f4e21203f2af275c765886ae87a352cd1
druvamani9844 <85678042+druvamani9844@users.noreply.github.com>
2021-11-06 21:05:35 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  
+ 
+}
+
+
+function draw(){
+  background("pink");
+
+}
